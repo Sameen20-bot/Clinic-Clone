4dfce37f60a4da2d9f88c3165bfefccbf4b0ee2e
Muhammad Ali Rasheed <alirasheedmd@gmail.com>
2024-10-10 17:33:14 +0500

    Fix Navbar open close.

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,11 +1,12 @@
-function close() {
-  // const element = document.getElementById("links-container");
-  // element.classList.add("close");
-  console.log("click");
+function myOpen() {
+  const element = document.getElementById("links-container")
+  element.classList.add("open")
+  console.log("click open")
 }
 
-function open() {
-  // const element = document.getElementById("links-container");
-  // element.classList.add("open");
-  console.log("click");
+function myClose() {
+  const element = document.getElementById("links-container")
+  element.classList.remove("open")
+
+  console.log("click close")
 }
